be167dd7837926530becd041c3255becc0ef62fb
Vaishnavi Kumbhakarna <80633315+vaishnavi1902@users.noreply.github.com>
2023-08-30 14:11:10 +0000

    control flow statements

---
+++ 06_control_flow.js
@@ -0,0 +1,88 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+if ( temperature === 40 ){
+    console.log("less than 50");
+} else {
+    console.log("temperature is greater than 50");
+}
+
+console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+const score = 200
+
+if (score > 100) {
+    let power = "fly"
+    console.log(`User power: ${power}`);
+}
+
+// console.log(`User power: ${power}`);
+
+
+const balance = 1000
+
+balance > 500 ? console.log("greater") : console.log("lower");
+
+if (balance > 500) console.log("test"),console.log("test2");
+
+if (balance < 500) {
+    console.log("less than 500");
+} else if (balance < 750) {
+    console.log("less than 750");
+    
+} else if (balance < 900) {
+    console.log("less than 750");
+    
+} else {
+    console.log("less than 1200");
+
+}
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
+
+// switch statement 
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+// if true then execute
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
