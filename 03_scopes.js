b6536901cf8b631a79f6145e955e0b08f82a6d87
Vaishnavi Kumbhakarna <80633315+vaishnavi1902@users.noreply.github.com>
2023-08-30 10:30:05 +0000

    functions

---
+++ 03_scopes.js
@@ -0,0 +1,31 @@
+const a = 90
+let p = 78
+var c = 72
+
+if(true){
+    var c = 89
+    let p = 3
+    console.log(p) 
+}
+console.log(p)
+
+function one () {
+    const name = "vaishnavi"
+    const age = 89
+    function two() {
+        const name = "vaishu"
+        const weight = 34
+        console.log(name)
+        console.log(age)
+    }
+    // console.log(weight) //error
+    console.log(name)
+    two()
+}
+one()
+
+const myfun =  function(myname) {
+    console.log(`my name is ${myname}`)
+}
+myfun("vaishnavi k . ")
+
