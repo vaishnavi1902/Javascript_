6862f868144d091d4edb805c16582261b4bfe023
Vaishnavi Kumbhakarna <80633315+vaishnavi1902@users.noreply.github.com>
2023-09-10 13:52:54 +0000

    Closures

---
+++ 16_closures.js
@@ -0,0 +1,39 @@
+function x(){
+    let p = 10 ; 
+    function y(){
+        let q = 20 ;
+        function z(){
+            let r = 30 ;
+            console.log(`Z executed ${p} ,${q} ,${r}`);
+        }
+        console.log(`Y executed ${p} ,${q}`);
+        z();
+        // console.log("Y executed");
+    }
+    console.log(`X executed ${p}`);
+    y();
+    // console.log("X executed");
+}
+x();
+
+function add() {
+    a = 7;
+    return  () => a + a;
+    
+}
+let c = add();
+console.log(c());
+// ----------------------------------------------
+const counts = ( function () {
+    let counter = 0;
+    return function () {
+        counter += 1; 
+        return counter;
+    }
+})();
+console.log(counts());
+console.log(counts());
+console.log(counts());
+console.log(counts());
+console.log(counts());
+
